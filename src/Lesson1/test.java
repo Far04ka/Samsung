3e5f9452424fc7aa5cce242f9a54829c51e69bac
Far04ka <deemen987@gmail.com>
2021-09-19 14:09:29 +0300

    123

--- src/Lesson1/test.java
+++ src/Lesson1/test.java
@@ -2,7 +2,7 @@ package Lesson1;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello world 123 i love samsung so much");
+        System.out.println("Hello world");
         
     }
 }
