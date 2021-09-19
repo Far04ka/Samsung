1958635b3f226e61db3be09a7c3e7e4b49220e24
Far04ka <68707207+Far04ka@users.noreply.github.com>
2021-09-19 14:09:49 +0300

    Update test.java

--- src/Lesson1/test.java
+++ src/Lesson1/test.java
@@ -2,7 +2,7 @@ package Lesson1;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("123");
         
     }
 }
