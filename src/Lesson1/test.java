4a630134bc6e91c7cdab0e966ca0967eb814bf2a
Far04ka <deemen987@gmail.com>
2021-09-20 22:43:57 +0300

    123

--- src/Lesson1/test.java
+++ src/Lesson1/test.java
@@ -1,8 +1,11 @@
 package Lesson1;
 
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("123");
-        
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt(), b = in.nextInt();
+        System.out.println((a*a+b*b));
     }
 }
