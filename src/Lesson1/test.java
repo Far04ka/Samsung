a69f1f022e5cfe0a272e5bbe42817f9454bd90f6
CastomPC <deemen987@gmail.com>
2021-09-25 01:43:21 +0300

    123

--- src/Lesson1/test.java
+++ src/Lesson1/test.java
@@ -1,11 +1,27 @@
 package Lesson1;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt(), b = in.nextInt();
-        System.out.println((a*a+b*b));
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);;
+        int arr [] = new int[3], couner=0;
+        for (int i = 0; i < 3; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int i = 0; i < 3; i++) {
+            if(arr[i] % 2 == 0){
+                couner++;
+            }
+        }
+        if(couner >=2){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+
+
     }
 }
